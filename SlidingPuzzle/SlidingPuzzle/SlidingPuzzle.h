abef7ec8da6d0f85d868c2fdec0ecdc9164a8bd1
nat7611 <nat7611@g.rit.edu>
2018-09-12 13:39:09 -0400

    Sliding puzzle beginnings.

---
+++ SlidingPuzzle/SlidingPuzzle/SlidingPuzzle.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#pragma region Variables
+int board[];
+int sideLength = 0;
+int boardSize;
+#pragma endregion
+
+#pragma region Functions
+void ScramblePuzzle();
+void DisplayPuzzle();
+bool CheckSolved();
+#pragma endregion
