2473f60586b1a029d693de7717deb3c8d812d0e6
nat7611 <nat7611@g.rit.edu>
2018-09-17 21:32:36 -0400

    A* continues to be broken, so gonna save a checkpoint and work on hill-climbing so I can get at least partial credit.

--- SlidingPuzzle/SlidingPuzzle/BoardManager.cpp
+++ SlidingPuzzle/SlidingPuzzle/BoardManager.cpp
@@ -5,11 +5,14 @@
 BoardManager::BoardManager()
 {
 	puzzle = new Puzzle(1);
+	path = new vector<Puzzle*>();
 }
 
 BoardManager::~BoardManager()
 {
 	delete puzzle;
+	delete path;
+	delete[] finalPath;
 }
 #pragma endregion
 
@@ -18,38 +21,33 @@ void BoardManager::SetPuzzle(int dimension) {
 	delete puzzle;
 	puzzle = new Puzzle(dimension);
 }
-
 void BoardManager::ScramblePuzzle() {
-	vector<Puzzle> moves;
+	vector<Puzzle*> moves;
 	int nextMove;
 	//Loop that makes a random amount of random moves
-	for (int i = 0; i < rand() % 64 + 128; i++) {
-		moves = AdjacencyDistances(puzzle->GetEmpty()->position);
-		nextMove = rand() % moves.size;
-		*puzzle = moves[nextMove];
+	for (int i = 0; i < 128; i++) {
+		moves = *AdjacencyDistances(puzzle);
+		nextMove = rand() % moves.size();
+		puzzle = moves[nextMove];
 	}
 }
-
 void BoardManager::DisplayPuzzle() {
-	cout << "-------------" << endl;
-	for (int i = 0; i < puzzle->boardSize; i += puzzle->sideLength) {
-		for (int j = 0; j < puzzle->sideLength; j++) {
-			cout << "|";
-			if (i + j + 1 < 10) cout << " ";
-			if (i + j + 1 == puzzle->boardSize) cout << "  ";
-			else cout << puzzle->board[i + j]->number;
-		}
-		cout << "|" << endl;
-	}
-	cout << "-------------" << endl;
+	puzzle->Display();
 }
 
+//Algorithm stuff
+void BoardManager::SolveAStar() {
+	AStarPath(puzzle);
+	for (int i = 0; i < finalPathLength; i++) {
+		int move = finalPath[i];
+		cout << "Move tile " << puzzle->board[finalPath[i]]->number;
+		MakeMove(move);
+		DisplayPuzzle();
+	}
+}
 void BoardManager::SetShortestPath()
 {
-	if (pathLength > finalPathLength)
-	{
-		return;
-	}
+	if (pathLength > finalPathLength) return;
 
 	if (finalPath != nullptr) //Deletes previous shortest path if it exists
 	{
@@ -58,23 +56,26 @@ void BoardManager::SetShortestPath()
 	//Creates and populates array with coordinates of path's vertices
 	finalPathLength = pathLength;
 	finalPath = new int[finalPathLength];
-	for (int i = 0; i < pathLength; i++) {
-		finalPath[i] = (*path)[i]->position;
+	for (int i = 1; i < pathLength; i++) { //i=1 is so finalPath doesn't include the starting board - that's still stored in puzzle.
+		Puzzle stepBoard = *(*path)[i];
+		finalPath[i] = stepBoard.emptyIndex;
 	}
 }
 #pragma endregion
 
 #pragma region Solution finding
-int BoardManager::AStarPath(int index)
+int BoardManager::AStarPath(Puzzle* move)
 {
-	Vertex* vertex = puzzle->board[index];
-	if ((*vertex).visited < pathLength - 3) { //Spot has been empty in the last three moves (only possible if it was just moved into or a square is about to be made)
+	move->Display();
+	Vertex* vertex = move->GetEmpty();
+	if ((*vertex).visited > pathLength - 3) { //Spot has been empty in the last three moves (only possible if it was just moved into or a square is about to be made)
 		return INT_MAX;
 	}
 	//Add vertex to path
-	(*path).push_back(vertex);
+	(*path).push_back(move);
 	pathLength++;
-	if (CheckProgress(path->data) == puzzle->boardSize) { //Reached end
+	//cout << CheckProgress(path->back()) << endl;
+	if (CheckProgress(path->back()) == 0) { //Reached end
 		SetShortestPath();
 		(*path).pop_back();
 		pathLength--;
@@ -84,54 +85,60 @@ int BoardManager::AStarPath(int index)
 	//Recursively finds next step, in order of distance from the endpoint
 	int curPath = INT_MAX;
 	int shortPath = INT_MAX;
-	for (Puzzle neighbor : AdjacencyDistances(index)) {
-		curPath = AStarPath(neighbor.emptyIndex);
+	vector<Puzzle*>* neighbors = AdjacencyDistances(move);
+	for (Puzzle* neighbor : *neighbors) {
+		curPath = AStarPath(neighbor);
 		if (curPath < shortPath) shortPath = curPath;
+		else delete neighbor;
 	}
-	//Removes vertex from path once we're done with it
+	//Clean-up
+	delete neighbors;
 	(*path).pop_back();
 	pathLength--;
 	return shortPath;
 }
 
 //Calculates the closeness of each move into the empty position and returns it
-vector<Puzzle> BoardManager::AdjacencyDistances(int index) {
-	vector<Puzzle> neighbors(0);
+vector<Puzzle*>* BoardManager::AdjacencyDistances(Puzzle* move) {
+	vector<Puzzle*>* neighbors = new vector<Puzzle*>(0);
+	int index = move->emptyIndex;
 	//Finds valid neighbors
 	if (index % puzzle->sideLength < puzzle->sideLength - 1) { //Right
-		neighbors.push_back(SimulateMove(index + 1));
+		neighbors->push_back(SimulateMove(move, index + 1));
 	}
 	if (index / puzzle->sideLength > 0) { //Up
-		neighbors.push_back(SimulateMove(index - puzzle->sideLength));
+		neighbors->push_back(SimulateMove(move, index - puzzle->sideLength));
 	}
 	if (index % puzzle->sideLength > 0) { //Left
-		neighbors.push_back(SimulateMove(index - 1));
+		neighbors->push_back(SimulateMove(move, index - 1));
 	}
 	if (index / puzzle->sideLength != puzzle->sideLength - 1) { //Down
-		neighbors.push_back(SimulateMove(index + puzzle->sideLength));
+		neighbors->push_back(SimulateMove(move, index + puzzle->sideLength));
 	}
-	sort(neighbors.begin(), neighbors.end(), sortMove); //Sorts list in ascending order by heuristics value
+	sort(neighbors->begin(), neighbors->end(), sortMove); //Sorts list in ascending order by heuristics value
 	return neighbors;
 }
 
-Puzzle BoardManager::SimulateMove(int moveIndex) {
-	Puzzle newPuzzle = *puzzle;
-	Vertex* tempVertex = newPuzzle.board[newPuzzle.emptyIndex];
-	newPuzzle.board[newPuzzle.emptyIndex] = newPuzzle.board[moveIndex];
-	newPuzzle.board[moveIndex] = tempVertex;
-	newPuzzle.emptyIndex = moveIndex;
+void BoardManager::MakeMove(int moveIndex) { //Wrapper function lel
+	puzzle = SimulateMove(puzzle, moveIndex);
+}
+Puzzle* BoardManager::SimulateMove(Puzzle* moveBoard, int moveIndex) {
+	Puzzle* newPuzzle = new Puzzle(*moveBoard);
+	newPuzzle->GetEmpty()->number = newPuzzle->board[moveIndex]->number;
+	newPuzzle->board[moveIndex]->number = 16;
+	newPuzzle->emptyIndex = moveIndex;
 	return newPuzzle;
 }
 
-bool sortMove(const Puzzle a, const Puzzle b) {
+bool sortMove(Puzzle* const a, Puzzle* const b) {
 	return CheckProgress(a) < CheckProgress(b);
 }
 
-//Returns how many tiles are in their final position
-int CheckProgress(Puzzle checkBoard) {
+//Returns the collective distance from the current tile state to the final one
+int CheckProgress(Puzzle* checkBoard) {
 	int closeness = 0;
-	for (int i = 0; i < checkBoard.boardSize; i++) { //Checks that all numbers are in place (last number represents the empty space)
-		if (checkBoard.board[i]->number == i + 1) closeness++;
+	for (int i = 0; i < checkBoard->boardSize; i++) { //Checks that all numbers are in place (last number represents the empty space)
+		closeness += abs(checkBoard->board[i]->number - i - 1) / checkBoard->sideLength + abs(checkBoard->board[i]->number - i - 1) % checkBoard->sideLength;
 	}
 	return closeness;
 }
--- SlidingPuzzle/SlidingPuzzle/Puzzle.h
+++ SlidingPuzzle/SlidingPuzzle/Puzzle.h
@@ -1,17 +1,24 @@
 #pragma once
 #include "Vertex.h"
 
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
 class Puzzle
 {
 public:
 	Puzzle(int dimension = 1);
 	~Puzzle();
+	Puzzle(const Puzzle& other); //Copy constructor
+	Puzzle& operator= (const Puzzle& other); //Copy assignment
 
 	Vertex** board;
 	int sideLength;
 	int boardSize;
 	int emptyIndex;
 
+	void Display();
 	Vertex* GetEmpty();
 };
 
